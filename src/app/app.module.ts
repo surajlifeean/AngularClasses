57975d34b74fa38b07bc7f4633ca88ff4dc835cb
SURAJ JESWARA <surajjeswara@yahoo.com>
2017-08-29 20:50:56 +0530

    route 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BlueColorDirective } from './blue-color.directive';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const appRoutes: Routes = [
+  { path: 'dashboard', 
+  component: DashboardComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BlueColorDirective
+    BlueColorDirective,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+     RouterModule.forRoot(
+      appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
